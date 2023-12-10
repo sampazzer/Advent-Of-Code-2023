c9d9f2b9d15049d7dbd995e5544c6494c543bf30
Sam Parry <sampazzer@googlemail.com>
2023-12-10 13:23:33 +0000

    added challenge texts

--- Day2/day2_1.py
+++ Day2/day2_1.py
@@ -10,21 +10,22 @@ index 1 = string with all the grabs
 index 3 = list with per grab (grab 1 is index 0 within this list)
 """
 # final list = list for getting the numbers of games that didnt have over the amount.
+# fmtdList is the main list keeping track of the split up data
 finalList = []
 fmtdList = []
 
-
+# the input file string
 input = "input2_1"
 
+# open the input file and read it line by line
 with open(input, "r") as f:
     lines = f.read().splitlines()
 
+# enumerated the input list to index the game numbers. Made it start from 1 rather than 0
 enlist = list(enumerate(lines, 1))
 
 for i, s in enlist:
     allnumberscolours = []
-    # print(i)
-    # print(s)
     findColon = s.find(":")
     strippedString = s[findColon + 2 :]
     perGrab = strippedString.split(";")
