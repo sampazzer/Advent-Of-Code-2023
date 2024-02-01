905e448c69ddf47d4e5871dd96c2199b9d8bb916
Sam Parry <sampazzer@googlemail.com>
2024-02-01 14:47:31 +0000

    Finished part 2!

--- Day4/day4_2.py
+++ Day4/day4_2.py
@@ -1,4 +1,4 @@
-input = "check_input"
+input = "input4_1"
 
 list_with_number_sets = []
 
@@ -21,7 +21,6 @@ for i in lines:
     ]
     # Split the string on space to get the separate numbers.
     list_first_nums = stripped_i_first_nums.split()
-    print(list_first_nums)
 
     # SECOND SET OF NUMBERS
     # Finding start position for the second set of numbers.
@@ -30,7 +29,6 @@ for i in lines:
     stripped_i_second_nums = i[position_of_separator_and_space_2:]
     # Split the string on space to get the separate numbers.
     list_second_nums = stripped_i_second_nums.split()
-    print(list_second_nums)
 
     # Comparing numbers in first list to second list. Adding them to an overall list if there are any matches.
     match_count = 0
@@ -39,30 +37,18 @@ for i in lines:
             match_count += 1
 
     list_with_number_sets.append(match_count)
-    print(list_with_number_sets)
-
-"""
-list_with_number_sets:
-     1  2  3  4  5  6
-    [4, 2, 2, 1, 0, 0]
-card_duplicates:
-    [1, 1, 1, 1, 1, 1]
-    [1, 2, 4, 4, 2, 1]
-
-final result should be:
-    [1, 2, 4, 8, 14, 1]
-"""
 
 print(list_with_number_sets)
-# Need to find which card has got the matching numbers so that I can understand which extra ones are going to be created. can do this from an enumeration+1
 
-# The for loop will take the current index i1 from list_with_number_sets A.
-# Nested for loop to take index i1
-# It adds one to the current index i2 in card_duplicates B.
-
-# Gets amount of matching numbers
-print(card_duplicates)
-
+# For each of the numbers in the matches list
 for index, numbers in enumerate(list_with_number_sets):
-    for locations in range(numbers):
-        card_duplicates[index + 1]
+    # Repeat another loop the the amount of time we get from the match numbers
+    for numbers2 in range(numbers):
+        # Get the number from the current index of card duplicates
+        number_buffer = card_duplicates[index]
+        # Add that number to the next numbers in the array
+        card_duplicates[index + numbers2 + 1] += number_buffer
+
+print(sum(card_duplicates))
+
+# Result was: 10425665
