8dadc1d32b1bc8b992af6d2450a729cf5e444efb
Sam Parry <sampazzer@googlemail.com>
2024-07-17 17:57:52 +0100

    adding more stuff

--- Day5/day5_1.py
+++ Day5/day5_1.py
@@ -1,13 +1,15 @@
 input = "check_input"
 
-# open the input file and read it line by line.
+# Open the input file and read it line by line.
 with open(input, "r") as f:
     lines = f.read().splitlines()
 
-print("LINES:")
-print(lines)
+# Get seeds from the list.
+seed_list_with_title = lines[0].split()
 
-newlines = [x for x in lines if x != ""]
+# Strip out the title.
+seed_list = seed_list_with_title[1:]
+print(seed_list)
 
-print("NEWLINES:")
-print(newlines)
+# Seed to soil maps:
+seed_to_soil_maps = lines[]
