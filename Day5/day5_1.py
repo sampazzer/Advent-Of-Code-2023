e4ec0b698d485db389a6bab01650b46060828873
Sam Parry <sampazzer@googlemail.com>
2024-07-17 18:26:09 +0100

    added to day 5

--- Day5/day5_1.py
+++ Day5/day5_1.py
@@ -12,4 +12,31 @@ seed_list = seed_list_with_title[1:]
 print(seed_list)
 
 # Seed to soil maps:
-seed_to_soil_maps = lines[]
+seed_to_soil_maps = lines[3:5]
+
+# Soil to fertilizer maps:
+soil_to_fertilizer_maps = lines[7:10]
+
+# Fertilizer to water maps:
+fertilizer_to_water_maps = lines[12:16]
+
+# Water to light maps:
+water_to_light_maps = lines[18:20]
+
+# Light to temperature maps:
+light_to_temperature_maps = lines[22:25]
+
+# Temperture to humidity maps:
+temperature_to_humidity_maps = lines[27:29]
+
+# Humidity to location maps:
+humidity_to_location_maps = lines[31:33]
+
+
+# print(seed_to_soil_maps)
+# print(soil_to_fertilizer_maps)
+# print(fertilizer_to_water_maps)
+# print(water_to_light_maps)
+# print(light_to_temperature_maps)
+# print(temperature_to_humidity_maps)
+# print(humidity_to_location_maps)
